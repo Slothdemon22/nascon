16ce6fbdc02ee10cbf730cb1db160719040ed57d
usemeastrashcan <ahmadhasnat490@gmail.com>
2025-04-19 10:34:30 +0500

    Juggar Commit 1 skibidi

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String },
-  createdAt: { type: Date, default: Date.now }
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true, required: true },
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  role: { type: String, enum: ['student', 'tutor'], default: 'student' },
+  coursesEnrolled: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Enrollment' }],
 });
 
-export default mongoose.models.User || mongoose.model('User', userSchema);
+export default mongoose.models.User || mongoose.model('User', UserSchema);
